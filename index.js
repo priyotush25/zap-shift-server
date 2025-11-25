53e485cf74e5bdbd350dd4e7dfe98bd2d248e4e4
Priyotush <priyotushpaul40@gmail.com>
2025-11-25 20:43:50 +0600

    single parcel add to payment

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelCollection.findOne(query);
+      res.send(result);
+    });
+
     // create parcel
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
